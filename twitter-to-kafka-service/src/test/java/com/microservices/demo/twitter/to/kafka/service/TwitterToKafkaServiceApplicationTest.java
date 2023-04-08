9ab705acc9003105d34eb2d696ef3e65b7028038
Joel Raj <joelanilraj2410@gmail.com>
2023-04-07 23:56:02 -0400

    MAIN : Added first microservice

---
+++ twitter-to-kafka-service/src/test/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplicationTest.java
@@ -0,0 +1,11 @@
+package com.microservices.demo.twitter.to.kafka.service;
+
+import org.junit.jupiter.api.Test;
+
+public class TwitterToKafkaServiceApplicationTest {
+
+    @Test
+    public void contextLoad() {
+
+    }
+}
